80d717b0e8497eb565d06cb04c5e2076e15c0e8b
MoisesJAV <moises18242@gmail.com>
2022-02-23 19:34:26 -0500

    S3-P2:Update pagina personal

--- semana-4/proyecto-javascript/js/index.js
+++ semana-4/proyecto-javascript/js/index.js
@@ -1,15 +1,25 @@
-'use strict';
-const buttomYes = window.document.getElementById('buttomYes');
-const buttomNo = document.querySelector('#buttomNo');
-const zomosNobios = () => {
-  alert('💞NoW ZoMos NoBioS💞');
-  alert('😱NuesTra VoDa Is TomorroW😱');
-  location.href = 'https://www.youtube.com/watch?v=QcQ-PxUtgaE';
-}
-const ebitarKCRombaMyBobo = () => {
-buttomNo.style.position = 'absolute';
-  buttomNo.style.top = (Math.random() * window.innerHeight) + 'px';
-  buttomNo.style.left = (Math.random() * window.innerWidth) + 'px';
-}
-buttomYes.addEventListener('click', zomosNobios);
-buttomNo.addEventListener('mouseover', ebitarKCRombaMyBobo);
+"use strict";
+const documentReady = () => {
+  const buttomYes = window.document.getElementById("buttomYes");
+  const buttomNo = document.querySelector("#buttomNo");
+
+  // const nombre = prompt('Komo TE LLaMAs bBY?');
+  // mainTitle.innerHTML += nombre + '❤';
+
+  const zomosNobios = () => {
+    alert("💞NoW ZoMos NoBioS💞");
+    alert("😱NuesTra VoDa Is TomorroW😱");
+    location.href = "https://www.youtube.com/watch?v=QcQ-PxUtgaE";
+  };
+  const ebitarKCRombaMyBobo = () => {
+    buttomNo.style.position = "absolute";
+    buttomNo.style.top = Math.random() * window.innerHeight + "px";
+    buttomNo.style.left = Math.random() * window.innerWidth + "px";
+  };
+  buttomYes.addEventListener("click", zomosNobios);
+  buttomNo.addEventListener("mouseover", ebitarKCRombaMyBobo);
+
+  const numeros = [4, 7, 9, 5, 3];
+  console.log(numeros.indexOf(7));
+};
+document.addEventListener("DOMContentLoaded", documentReady);
