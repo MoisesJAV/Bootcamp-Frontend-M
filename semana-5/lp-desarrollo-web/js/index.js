c98f67b1f3f82d4168a0045505eddab1825e78d2
MoisesJAV <moises18242@gmail.com>
2022-03-01 17:35:10 -0500

    Update LP Desarrollo web

--- semana-5/lp-desarrollo-web/js/index.js
+++ semana-5/lp-desarrollo-web/js/index.js
@@ -2,14 +2,20 @@
 const documentReady = () => {
 
   const header = document.querySelector('.header');
+  const headerNav = document.querySelector('.header-nav');
   const headerNavMenuIconContainer = document.getElementById('headerNavMenuIconContainer');
   const headerNavMenuIcon = document.querySelector('.header-nav__menu-icon');
   const headerNavCloseIconContainer = document.getElementById('headerNavCloseIconContainer');
   const headerNavLinkList = document.querySelector('.header-nav__link-list');
+  const headerNavLinks = [...document.querySelectorAll('.header-nav__link')];
 
-  const windowScroll = () => {
+  const documentScroll = () => {
     header.classList.toggle('header--scroll', window.scrollY > 0);
+    headerNav.classList.toggle('header-nav--scroll', window.scrollY > 0);
     headerNavMenuIcon.classList.toggle('header-nav__menu-icon--scroll', window.scrollY > 0);
+    headerNavLinks.map((element) => {
+      element.classList.toggle('header-nav__link--scroll', window.scrollY > 0);
+    });
   };
 
   const openMenu = () => {
@@ -20,8 +26,8 @@ const documentReady = () => {
     headerNavLinkList.classList.remove('header-nav__link-list--open');
   };
 
-  window.addEventListener('scroll', windowScroll);
-  headerNavMenuIconContainer.addEventListener('click');
+  document.addEventListener('scroll', documentScroll);
+  headerNavMenuIconContainer.addEventListener('click', openMenu);
   headerNavCloseIconContainer.addEventListener('click', closeMenu);
 
 }
